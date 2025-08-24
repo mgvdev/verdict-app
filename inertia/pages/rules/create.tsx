4590e416f51981b9375c701786e0c03e03194674
mgvdev <maxence.guyonvarho@gmail.com>
2025-08-24 17:06:52 +0200

    feat(builder): add text input for rule name

--- inertia/pages/rules/create.tsx
+++ inertia/pages/rules/create.tsx
@@ -1,16 +1,34 @@
 import { AuthLayout } from '~/layouts/authLatout'
-import { ReactNode } from 'react'
+import { ReactNode, useState } from 'react'
 import VerdictStudio from '~/components/rules/ruleBuilder/ruleBuilder'
-
+import { Button, Card, Group, Text, TextInput } from '@mantine/core'
 
 function Create() {
+  const [rules, setRules] = useState({})
+  const [name, setName] = useState('')
 
   return (
     <div>
-      <VerdictStudio/>
+      <Card withBorder>
+        <Group justify="space-between" align="center">
+          <Group w={'70%'}>
+            <TextInput
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              withAsterisk
+              placeholder="Rule name"
+              w={'100%'}
+            />
+          </Group>
+          <Group>
+            <Button color="blue">Save</Button>
+            <Text size={'xs'} c={'gray'}>Last save at </Text>
+          </Group>
+        </Group>
+      </Card>
+      <VerdictStudio onChange={(value) => setRules(value)} />
     </div>
   )
-
 }
 
 Create.layout = (page: ReactNode) => <AuthLayout>{page}</AuthLayout>
